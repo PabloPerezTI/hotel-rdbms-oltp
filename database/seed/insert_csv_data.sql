8c4c2cab5910d05da8e1d2ba2c7908d27a8f27f9
Pablo Perez <pabloperezorti@gmail.com>
2025-05-19 13:54:51 +0200

    insert_csv_data.sql fixed

--- database/seed/insert_csv_data.sql
+++ database/seed/insert_csv_data.sql
@@ -1,7 +1,17 @@
-\copy huesped FROM 'data/T_huesped.csv' WITH (FORMAT csv, HEADER true);
-\copy tipos_habitacion FROM 'data/T_tipos_hab.csv' WITH (FORMAT csv, HEADER true);
-\copy habitaciones FROM 'data/T_habitaciones.csv' WITH (FORMAT csv, HEADER true);
-\copy reservas FROM 'data/T_reservas.csv' WITH (FORMAT csv, HEADER true);
-\copy tipos_servicios FROM 'data/T_tipos_servicio.csv' WITH (FORMAT csv, HEADER true);
-\copy reserva_servicio FROM 'data/T_reserva_servicio.csv' WITH (FORMAT csv, HEADER true);
-\copy pagos FROM 'data/T_pago.csv' WITH (FORMAT csv, HEADER true);
+SET datestyle = 'DMY';
+
+TRUNCATE TABLE huesped CASCADE;
+TRUNCATE TABLE tipos_habitacion CASCADE;
+TRUNCATE TABLE habitaciones CASCADE;
+TRUNCATE TABLE reservas CASCADE;
+TRUNCATE TABLE tipos_servicios CASCADE;
+TRUNCATE TABLE reserva_servicio CASCADE;
+TRUNCATE TABLE pagos CASCADE;
+
+\copy huesped FROM '/tmp/data/T_huesped.csv' WITH (FORMAT csv, HEADER true);
+\copy tipos_habitacion FROM '/tmp/data/T_tipos_hab.csv' WITH (FORMAT csv, HEADER true);
+\copy habitaciones FROM '/tmp/data/T_habitaciones.csv' WITH (FORMAT csv, HEADER true);
+\copy reservas FROM '/tmp/data/T_reservas.csv' WITH (FORMAT csv, HEADER true);
+\copy tipos_servicios FROM '/tmp/data/T_tipos_servicio.csv' WITH (FORMAT csv, HEADER true);
+\copy reserva_servicio FROM '/tmp/data/T_reserva_servicio.csv' WITH (FORMAT csv, HEADER true);
+\copy pagos FROM '/tmp/data/T_pago.csv' WITH (FORMAT csv, HEADER true);
